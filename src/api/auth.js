6e1da147c451c85cb1955bef90791507d28f4074
Phatchathorn P <ponypingpong@gmail.com>
2025-01-14 13:03:24 +0700

    Add access and refresh token function

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,38 +1,97 @@
-import {
-  SignInResponse,
-  getCsrfToken,
-  getSession,
-  signIn,
-  signOut,
-} from "next-auth/react";
-import { axiosAuth, axiosDefault } from "./axios-instance";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-const BASE_PATH = process.env.BASE_PATH || "/";
-
-export const googleSignIn = async () => {
-  await signIn("google", { callbackUrl: `${BASE_PATH}` });
-};
-
-export const signOutApi = async () => {
-  await signOut({ callbackUrl: `${BASE_PATH}` });
-  await axiosAuth.post("/auth/logout");
-};
-
-export const refreshAccessToken = async (refreshToken) => {
-  const tokens = await axiosDefault.post("auth/token", {
-    refresh_token: refreshToken,
+const GOOGLE_AUTHORIZATION_URL =
+  "https://accounts.google.com/o/oauth2/v2/auth?" +
+  new URLSearchParams({
+    prompt: "consent",
+    access_type: "offline",
+    response_type: "code",
   });
 
-  const csrfToken = await getCsrfToken();
-  await getSession({
-    req: {
-      body: {
-        csrfToken,
-        data: {
-          accessToken: tokens.data.access_token,
-          refreshToken: tokens.data.refresh_token,
-        },
+/**
+ * Takes a token, and returns a new token with updated
+ * `accessToken` and `accessTokenExpires`. If an error occurs,
+ * returns the old token and an error property
+ */
+async function refreshAccessToken(token) {
+  try {
+    const url =
+      "https://oauth2.googleapis.com/token?" +
+      new URLSearchParams({
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        grant_type: "refresh_token",
+        refresh_token: token.refreshToken,
+      });
+
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
       },
+      method: "POST",
+    });
+
+    const refreshedTokens = await response.json();
+
+    if (!response.ok) {
+      throw refreshedTokens;
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorizationUrl: GOOGLE_AUTHORIZATION_URL,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+  callbacks: {
+    async jwt(token, user, account) {
+      // Initial sign in
+      if (account && user) {
+        return {
+          accessToken: account.accessToken,
+          accessTokenExpires: Date.now() + account.expires_in * 1000,
+          refreshToken: account.refresh_token,
+          user,
+        };
+      }
+
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        return token;
+      }
+
+      // Access token has expired, try to update it
+      return refreshAccessToken(token);
     },
-  });
+    async session(session, token) {
+      if (token) {
+        session.user = token.user;
+        session.accessToken = token.accessToken;
+        session.error = token.error;
+      }
+
+      return session;
+    },
+  },
 };
+export default NextAuth(authOptions);

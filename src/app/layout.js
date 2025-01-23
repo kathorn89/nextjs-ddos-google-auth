3f3fdf40a561c63d33349c6487ab8c7929b1d64d
Phatchathorn P <ponypingpong@gmail.com>
2025-01-23 17:32:13 +0700

    Add function disable right click

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect } from "react";
 import { Inter } from "next/font/google";
 import { Figtree } from "next/font/google";
 import { NextAuthProvider } from "./Providers";
@@ -12,6 +13,19 @@ const inter = Inter({ subsets: ["latin"] });
 const figtree = Figtree({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    const disableRightClick = (e) => {
+      e.preventDefault();
+    };
+
+    document.addEventListener("contextmenu", disableRightClick);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      document.removeEventListener("contextmenu", disableRightClick);
+    };
+  }, []);
+
   return (
     <html lang="en">
       <link rel="icon" href="/icon.png" sizes="any" />
